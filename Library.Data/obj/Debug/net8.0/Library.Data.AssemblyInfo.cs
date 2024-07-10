506e587d5e4cc30665d1cfaf095fca1f61c0aef5
GiorgiCoder <giorgiminishvili@gmail.com>
2024-07-10 17:29:45 +0400

    merging

--- Library.Data/obj/Debug/net8.0/Library.Data.AssemblyInfo.cs
+++ Library.Data/obj/Debug/net8.0/Library.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90cb16031d6fa7c5de24b7a4542342bebeafffa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4caf3fea62375b193e5efbc00cd4b21548f76bab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
